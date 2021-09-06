e262833562482e2483b9b89baa286b7781a2d7bd
Mikkel Malmberg <mikkel@brnbw.com>
2021-09-06 12:20:23 +0200

    Uhm, adding contracts (wip)

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,31 @@
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-etherscan");
+require("@nomiclabs/hardhat-waffle");
+// require("@nomiclabs/hardhat-ganache");
+
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(await account.address);
+  }
+});
+
 module.exports = {
-  solidity: "0.7.3",
+  solidity: "0.8.2",
+  // defaultNetwork: "localhost",
+  networks: {
+    ropsten: {
+      url: process.env.ROPSTEN_ENDPOINT,
+      accounts: [process.env.PRIVATE_KEY]
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };
